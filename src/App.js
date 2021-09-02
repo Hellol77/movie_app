839f9a960d82160f624b37fab7a5828d448cc248
Hellol77 <dhe7700@naver.com>
2021-09-02 22:19:58 +0900

    Dynamic component generation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from "react";
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "라면",
+    image:
+      "https://image.shutterstock.com/image-photo/steam-smoke-instant-noodles-bowl-600w-1939669045.jpg",
+  },
+  {
+    name: "치킨",
+    image:
+      "https://cdn.pixabay.com/photo/2015/03/11/00/31/chicken-667935_1280.jpg",
+  },
+  {
+    name: "피자",
+    image:
+      "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395__480.jpg",
+  },
+];
 function App() {
   return (
     <div>
-      <h1>hello</h1>
-      <Food fav="kimchi" />
-
-
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
